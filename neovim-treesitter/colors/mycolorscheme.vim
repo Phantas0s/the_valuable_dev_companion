7fee1f5516e3bbd48641514740152cfa0130f2b3
Matthieu <matthieu.cneude@gmail.com>
2023-04-10 18:23:32 +0200

    Files for next article

---
+++ neovim-treesitter/colors/mycolorscheme.vim
@@ -0,0 +1,2 @@
+highlight @conditional ctermfg=#00FF00 ctermbg=NONE cterm=NONE
+highlight @repeat ctermfg=#0000FF ctermbg=NONE cterm=NONE
---
+++ neovim-treesitter/example.lua
@@ -0,0 +1,62 @@
+#!/usr/bin/env lua
+
+local function basic_text_objects()
+    local chars = { '_', '.', ':', ',', ';', '|', '/', '\\', '*', '+', '%', '`', '?' }
+    for _,char in ipairs(chars) do
+        for _,mode in ipairs({ 'x', 'o' }) do
+            vim.keymap.set(mode, 'i'..char, string.format(':<C-u>silent! normal! f%sF%slvt%s<CR>', char, char, char), { silent = true })
+            vim.keymap.set(mode, 'a'..char, string.format(':<C-u>silent! normal! f%sF%svf%s<CR>', char, char, char, { silent = true }))
+        end
+    end
+end
+
+function select_indent(around)
+    local start_indent = vim.fn.indent(vim.fn.line('.'))
+    local blank_line_pattern = '^%s*$'
+
+    if string.match(vim.fn.getline('.'), blank_line_pattern) then
+        return
+    end
+
+    if vim.v.count > 0 then
+        start_indent = start_indent - vim.o.shiftwidth * (vim.v.count - 1)
+        if start_indent < 0 then
+            start_indent = 0
+        end
+    end
+
+    local prev_line = vim.fn.line('.') - 1
+    local prev_blank_line = function(line) return string.match(vim.fn.getline(line), blank_line_pattern) end
+    while prev_line > 0 and (prev_blank_line(prev_line) or vim.fn.indent(prev_line) >= start_indent) do
+        vim.cmd('-')
+        prev_line = vim.fn.line('.') - 1
+    end
+    if around then
+        vim.cmd('-')
+    end
+
+    vim.cmd('normal! 0V')
+
+    local next_line = vim.fn.line('.') + 1
+    local next_blank_line = function(line) return string.match(vim.fn.getline(line), blank_line_pattern) end
+    local last_line = vim.fn.line('$')
+    while next_line <= last_line and (next_blank_line(next_line) or vim.fn.indent(next_line) >= start_indent) do
+        vim.cmd('+')
+        next_line = vim.fn.line('.') + 1
+    end
+    if around then
+        vim.cmd('+')
+    end
+end
+
+local function indent_text_objects()
+    for _,mode in ipairs({ 'x', 'o' }) do
+        vim.keymap.set(mode, 'ii', ':<c-u>lua select_indent()<cr>', { silent = true })
+        vim.keymap.set(mode, 'ai', ':<c-u>lua select_indent(true)<cr>', { silent = true })
+    end
+end
+
+return {
+    basic_text_objects = basic_text_objects,
+    indent_text_objects = indent_text_objects,
+}
